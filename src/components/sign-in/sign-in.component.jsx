8221c0f67d46cfe9bfac48915dfee737eda44b18
David K <dkim@thomasnet.com>
2020-12-25 19:44:31 -0500

    Added sign in method with login and email

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,22 +1,28 @@
-import React, { Component } from "react";
-import "./sign-in.styles.scss";
+import React, { Component } from 'react';
+import './sign-in.styles.scss';
 
-import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   handleChange = (event) => {
