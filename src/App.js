904d52b6a9612d9290dfe3742f871e20ae3fbe43
David K <davidykim10@gmail.com>
2021-06-29 22:34:16 -0400

    updated file path

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { selectCurrentUser } from './redux/user/user.selector';
 import { setCurrentUser } from './redux/user/user.actions';
 
-import HomePage from './pages/homepage/homepagee.component';
+import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
