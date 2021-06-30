047940d578d030ce2606da02f8bc69a149220616
David K <davidykim10@gmail.com>
2021-06-29 22:40:04 -0400

    Fixed broken file path

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import CollectionPage from '../collection/collection';
+import CollectionPage from '../collection/collection.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 const ShopPage = ({ match }) => {
